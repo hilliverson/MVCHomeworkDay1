6e1a763e8810a82de43f639bb6d78f2445e6fadc
hilliverson@gmail.com <hilliverson@gmail.com>
2018-01-17 10:37:21 +0800

    first commit

---
+++ MVCHomeworkDay1/MVCHomeworkDay1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCHomeworkDay1.MvcApplication" Language="C#" %>
---
+++ MVCHomeworkDay1/MVCHomeworkDay1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCHomeworkDay1.Startup))]
+namespace MVCHomeworkDay1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
