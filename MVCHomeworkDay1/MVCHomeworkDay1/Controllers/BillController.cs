8bd58c2013d7c3ccfdcafe640f3d737937b430e7
hilliverson@gmail.com <hilliverson@gmail.com>
2018-01-17 22:36:31 +0800

    OK

---
+++ MVCHomeworkDay1/MVCHomeworkDay1/Controllers/BillController.cs
@@ -0,0 +1,31 @@
+﻿using MVCHomeworkDay1.Models;
+using MVCHomeworkDay1.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCHomeworkDay1.Controllers
+{
+    public class BillController : Controller
+    {
+        // GET: Bill
+        public ActionResult Bill()
+        {
+            List<Bill> list = new List<Bill>();
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                Bill obj = new Bill();
+                obj.type = "支出";
+                obj.money = random.Next(0, 10000);
+                obj.date = DateTime.Now.ToString("yyyy-mm-dd");
+                list.Add(obj);
+            }
+            BillViewModel model = new BillViewModel();
+            model.BillData = list;
+            return View(model);
+        }
+    }
+}
---
+++ MVCHomeworkDay1/MVCHomeworkDay1/Models/ViewModel/BillViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCHomeworkDay1.Models.ViewModel
+{
+    public class BillViewModel
+    {
+       
+        public List<Bill> BillData { get; set; }
+       
+    }
+}
