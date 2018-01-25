2a3931b3f06b67ed96feb40b3eff58f0eb12f1f8
hilliverson <hilliverson@gmail>
2018-01-25 15:39:16 +0800

    修改BillController增加轉換，新增AccountBookService讀取資料庫，新增AccountBook接受資料庫欄位資料

---
+++ MVCHomeworkDay1/MVCHomeworkDay1/Models/AccountBookService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+using MVCHomeworkDay1.Models.DataModel;
+using MVCHomeworkDay1.Models.ViewModel;
+
+public class AccountBookService
+{
+    private string _ConnStr;
+    public AccountBookService()
+	{
+        _ConnStr = @"(localdb)\MSSQLLocalDB;Initial Catalog=C:\USERS\HILL.HUANG\DOWNLOADS\SESSION2\HOMEWORKDB\HOMEWORKDB\APP_DATA\SKILLTREEHOMEWORK.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+	}
+
+    public List<AccountBook> Get()
+    {
+        using (var conn = new SqlConnection(_ConnStr))
+        {
+            List<AccountBook> list = conn.Query<AccountBook>("select * from AccountBook").ToList();
+            return list;
+        }
+    }
+}
---
+++ MVCHomeworkDay1/MVCHomeworkDay1/Models/DataModel/AccountBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCHomeworkDay1.Models.DataModel
+{
+    public class AccountBook
+    {
+        public int Id { get; set; }
+        public int Categoryyy { get; set; }
+        public int Amounttt { get; set; }
+        public DateTime Dateee { get; set; }
+        public string Remarkkk { get; set; }
+    }
+}
